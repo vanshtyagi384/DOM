62c43bb9335b9f6675938e2efaa40a946fe47b84
vanshtyagi384 <vanshtyagi.tyagi012@gmail.com>
2025-04-20 00:01:31 +0530

    Dom1

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function changeBackgorund(color){
 }
 
 
+const themeButton = document.getElementById("theme-button");
+
+
+themeButton.addEventListener('click', () => {
+    console.log(document.body.style.backgroundColor);
+    const currentColor = document.body.style.backgroundColor;
+
+    if(!currentColor || currentColor == 'white'){
+        changeBackgorund('black')
+        themeButton.innerText = 'LIGHT MODE'
+    }else{
+        changeBackgorund('white'),
+        themeButton.innerText = 'DARKMODE'
+    }
+    
+})
